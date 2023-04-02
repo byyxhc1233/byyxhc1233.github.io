5bd720a9a8ec2d2cb810efadd3f2bbeee95c605d
byyxhc1233 <1291986322@qq.com>
2023-04-02 22:21:17 +0800

    Site updated: 2023-04-02 22:21:16

--- js/cursor.js
+++ js/cursor.js
@@ -82,3 +82,24 @@ class Cursor {
     CURSOR = new Cursor();
     // 需要重新获取列表时，使用 CURSOR.refresh()
 })();
+
+
+/* 恶搞标题 start */
+//动态标题
+var OriginTitile = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+  if (document.hidden) {
+    //离开当前页面时标签显示内容
+    document.title = '👀跑哪里去了~';
+    clearTimeout(titleTime);
+  } else {
+    //返回当前页面时标签显示内容
+    document.title = '🐖抓到你啦～';
+    //两秒后变回正常标题
+    titleTime = setTimeout(function () {
+      document.title = OriginTitile;
+    }, 2000);
+  }
+});
+/* 恶搞标题 end */
